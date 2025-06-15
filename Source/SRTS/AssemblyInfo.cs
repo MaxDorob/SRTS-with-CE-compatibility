a6b7c02ff5a0c0af16cc029dc475a48b02d16709
MaxDorob <maxdor81@gmail.com>
2025-06-15 20:44:26 +0600

    1.6 project changes

--- Source/SRTS/AssemblyInfo.cs
+++ Source/SRTS/AssemblyInfo.cs
@@ -1,4 +1,14 @@
-﻿using System.Reflection;
+﻿#if RELEASE1_6
+global using TravelingTransportPods = RimWorld.Planet.TravellingTransporters;
+global using TransportPodsArrivalAction_LandInSpecificCell = RimWorld.Planet.TransportersArrivalAction_LandInSpecificCell;
+global using TransportPodsArrivalAction = RimWorld.Planet.TransportersArrivalAction;
+global using TransportPodsArrivalAction_FormCaravan = RimWorld.Planet.TransportersArrivalAction_FormCaravan;
+global using ActiveDropPodInfo = RimWorld.ActiveTransporterInfo;
+global using ActiveDropPod = RimWorld.ActiveTransporter;
+global using IActiveDropPod = RimWorld.IActiveTransporter;
+#endif
+using System.Reflection;
 using System.Runtime.InteropServices;
+
 
 [assembly: Guid("9c84a414-98d2-46f9-8469-4ebf0b48c014")]
