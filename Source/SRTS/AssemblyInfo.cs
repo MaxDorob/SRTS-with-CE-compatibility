54c214aebf9ffa0e2ae4b0385d9b63bf8ffa0203
MaxDorob <maxdor81@gmail.com>
2025-06-15 20:54:22 +0600

    And another missing type

--- Source/SRTS/AssemblyInfo.cs
+++ Source/SRTS/AssemblyInfo.cs
@@ -6,6 +6,7 @@ global using TransportPodsArrivalAction_FormCaravan = RimWorld.Planet.Transporte
 global using ActiveDropPodInfo = RimWorld.ActiveTransporterInfo;
 global using ActiveDropPod = RimWorld.ActiveTransporter;
 global using TransportPodsArrivalAction_VisitSite = RimWorld.Planet.TransportersArrivalAction_VisitSite;
+global using TransportPodsArrivalActionUtility = RimWorld.Planet.TransportersArrivalActionUtility;
 global using IActiveDropPod = RimWorld.IActiveTransporter;
 #endif
 using System.Reflection;
