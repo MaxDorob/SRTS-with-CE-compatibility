346bdb3c06f02ded42324b3cac60dc081d2778cd
MaxDorob <maxdor81@gmail.com>
2025-07-25 03:43:46 +0600

    Removed most obsolete classes Started global rework

--- Source/SRTS/CompProperties_LaunchableSRTS.cs
+++ Source/SRTS/CompProperties_LaunchableSRTS.cs
@@ -1,9 +1,42 @@
-﻿using Verse;
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
 
 namespace SRTS
 {
-    public class CompProperties_LaunchableSRTS : CompProperties
+    public class CompProperties_LaunchableSRTS : CompProperties_Shuttle
     {
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            foreach(var error in base.ConfigErrors(parentDef))
+            {
+                yield return error;
+            }
+            if (parentDef.GetCompProperties<CompProperties_Launchable>() == null)
+            {
+                yield return "There's no CompLaunchable";
+            }
+            if (shipDef == null)
+            {
+                yield return "shipDef is null";
+            }
+            else
+            {
+                if (shipDef.leavingSkyfaller?.skyfaller.rotationCurve == null)
+                {
+                    yield return $"{nameof(shipDef.leavingSkyfaller)}'s {shipDef.leavingSkyfaller.skyfaller.rotationCurve} is null";
+                }
+                if (shipDef.leavingSkyfaller?.skyfaller.speedCurve == null)
+                {
+                    yield return $"{nameof(shipDef.leavingSkyfaller)}'s {shipDef.leavingSkyfaller.skyfaller.speedCurve} is null";
+                }
+                if (shipDef.leavingSkyfaller?.skyfaller.zPositionCurve == null)
+                {
+                    yield return $"{nameof(shipDef.leavingSkyfaller)}'s {shipDef.leavingSkyfaller.skyfaller.zPositionCurve} is null";
+                }
+            }
+
+        }
         public CompProperties_LaunchableSRTS()
         {
             this.compClass = typeof (CompLaunchableSRTS);
