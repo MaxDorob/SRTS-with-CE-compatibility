dc2bc757a02771ea5344e3db963bd2e92d4e28f5
Макс Дороболюк <dmv_01@mail.ru>
2025-09-08 05:01:49 +0600

    Store mod version. Backward compatibility.

---
+++ Source/SRTS/LegacyBackCompatibilityConverter.cs
@@ -0,0 +1,78 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Verse;
+
+namespace SRTS
+{
+    [StaticConstructorOnStartup]
+    public class LegacyBackCompatibilityConverter : BackCompatibilityConverter
+    {
+        [HarmonyLib.HarmonyPatch(typeof(BackCompatibility), "CheckSaveIdenticalToCurrentEnvironment")]
+        internal static class CheckSaveIdenticalToCurrentEnvironment_Patch
+        {
+            public static void Postfix(ref bool __result)
+            {
+                if (__result && ModVersion.ActualVersion != ModVersion.SRTSInSave)
+                {
+                    Log.WarningOnce($"SRTS version mismatch:\nSave - {ModVersion.SRTSInSave}\nActual version - {ModVersion.ActualVersion}", 826317278);
+                    __result = false;
+                }
+            }
+        }
+        static LegacyBackCompatibilityConverter()
+        {
+            BackCompatibility.conversionChain.Add(new LegacyBackCompatibilityConverter());
+        }
+        public override bool AppliesToVersion(int majorVer, int minorVer)
+        {
+            return ModVersion.ActualVersion != ModVersion.SRTSInSave;
+        }
+
+        public override string BackCompatibleDefName(Type defType, string defName, bool forDefInjections = false, XmlNode node = null)
+        {
+            return null;
+        }
+
+        public override Type GetBackCompatibleType(Type baseType, string providedClassName, XmlNode node)
+        {
+            if (providedClassName == "Building")
+            {
+                var def = DefDatabase<ThingDef>.GetNamed(node["def"].InnerText);
+
+                if (def != null && def.thingClass.Name != providedClassName && def.HasComp<CompLaunchableSRTS>())
+                {
+                    return def.thingClass;
+                }
+            }
+            return null;
+        }
+
+        public override void PostExposeData(object obj)
+        {
+            if (obj is Building building)
+            {
+                var srtsComp = building.TryGetComp<CompLaunchableSRTS>();
+                if (srtsComp != null)
+                {
+                    if (srtsComp.shipParent == null)
+                    {
+                        srtsComp.PostSpawnSetup(false);
+                        srtsComp.requiredItems ??= new List<ThingDefCount>();
+                        srtsComp.requiredPawns ??= new List<Pawn>();
+                        srtsComp.pawnsToIgnoreIfDownedOfNotOnTheMap ??= new List<Pawn>();
+                        var transporterComp = building.TryGetComp<CompTransporter>();
+                        if (transporterComp != null)
+                        {
+                            transporterComp.leftToLoad ??= new List<TransferableOneWay>();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/SRTS/ModVersion.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SRTS
+{
+    public static class ModVersion
+    {
+        [HarmonyLib.HarmonyPatch(typeof(ScribeMetaHeaderUtility), nameof(ScribeMetaHeaderUtility.WriteMetaHeader))]
+        internal static class WriteModVersion_Patch
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (var instruction in instructions)
+                {
+                    if (instruction.opcode == OpCodes.Leave_S)
+                    {
+                        yield return CodeInstruction.Call(typeof(WriteModVersion_Patch), nameof(WriteModVersion_Patch.WriteSRTSVersion));
+                    }
+                    yield return instruction;
+                }
+            }
+            public static void WriteSRTSVersion()
+            {
+                try
+                {
+                    SRTSInSave = ActualVersion;
+                    Scribe_Values.Look(ref SRTSInSave, nameof(SRTSInSave));
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.Message);
+                }
+            }
+        }
+        [HarmonyLib.HarmonyPatch(typeof(ScribeMetaHeaderUtility), nameof(ScribeMetaHeaderUtility.LoadGameDataHeader))]
+        internal static class LoadModVersion_Patch
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var patched = false;
+                foreach (var instruction in instructions)
+                {
+                    if (!patched && instruction.opcode == OpCodes.Ldsflda)
+                    {
+                        yield return CodeInstruction.Call(typeof(LoadModVersion_Patch), nameof(LoadModVersion_Patch.LoadSRTSVersion));
+                        patched = true;
+                        Log.Message(nameof(ScribeMetaHeaderUtility.LoadGameDataHeader) + " was patched");
+                    }
+                    yield return instruction;
+                }
+            }
+            public static void LoadSRTSVersion()
+            {
+                try
+                {
+                    Scribe_Values.Look(ref SRTSInSave, nameof(SRTSInSave));
+                    Log.Message("SRTS version in save: " + SRTSInSave);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.Message);
+                }
+            }
+        }
+        public static string SRTSInSave = null;
+        public static string ActualVersion => "1.6.2.1";
+
+        
+    }
+}
