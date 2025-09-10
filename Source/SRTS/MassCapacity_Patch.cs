6d5d811c685f2ee6924ad5653cb686c20c498bdd
MaxDorob <maxdor81@gmail.com>
2025-09-11 01:47:32 +0600

    MassCapacity patch

---
+++ Source/SRTS/MassCapacity_Patch.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SRTS
+{
+    [HarmonyLib.HarmonyPatch(typeof(CompTransporter), nameof(CompTransporter.MassCapacity), HarmonyLib.MethodType.Getter)]
+    internal static class MassCapacity_Patch
+    {
+        public static bool Prefix(CompTransporter __instance, ref float __result)
+        {
+            if (__instance.parent.HasComp<CompLaunchableSRTS>())
+            {
+                __result = SRTSMod.GetStatFor<float>(__instance.parent.def.defName, StatName.massCapacity);
+                return false;
+            }
+            return true;
+        }
+    }
+}
